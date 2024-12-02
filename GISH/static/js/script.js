495e40051719283486d24e50fdee9b302455513c
Alan Maizon <alanmaizon@hotmail.com>
2024-12-02 23:48:13 +0000

    Changes to be committed:	modified:     GISH/orders/templates/orders/product_form.html 	modified:   GISH/static/js/script.js

--- GISH/static/js/script.js
+++ GISH/static/js/script.js
@@ -13,7 +13,7 @@ function init(containerId) {
     scene.background = new THREE.Color(0x111111);
 
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(0, 1, 10);
+    camera.position.set(0, 1, 4);
 
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
